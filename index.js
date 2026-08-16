ddfd50f078bcaec090a869d903ffa01618f8d64c
Rakibul Islam Jisan <rakibulislam2001gsc@gmail.com>
2026-08-16 22:14:23 +0600

    complete vercel config setup

--- index.js
+++ index.js
@@ -5,13 +5,15 @@ const router = require('./routes/authrouter')
 const ConnectDB = require('./config/ConnectDB')
 const app = express()
 
-
+app.set('trust proxy', 1);
 app.use(express.json())
 app.use('/api/auth',router)
 
 ConnectDB()
-
-app.listen(8000,()=>{
+const PORT = process.env.PORT || 8000;
+app.listen(PORT,()=>{
     console.log("server is running on port 8000");
     
 })
+
+module.exports = app;
